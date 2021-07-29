435b08427e0695fedbdd484ec187e2d91495fce5
OrtizAlex <46968556+OrtizAlex@users.noreply.github.com>
2021-07-29 09:29:35 -0400

    created routes. fixed model issue and server path

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
   day: {
-    type: date,
+    type: Date,
     default: () => new Date.now
   },
   exercises: [
---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+router.post("/api/workouts", ({body}, res) => {
+  Workout.create(body)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body }},
+    { new: true })
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+module.exports = router;
